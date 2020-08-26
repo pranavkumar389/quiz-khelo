945c083e0ea34211a0b860817f109e30f5fefd3e
pranavkumar389 <pk9work@gmail.com>
2020-08-26 18:32:48 +0530

    Updates

---
+++ precache-manifest.42fac2ad246d0054dade08f423f1900e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d08bc6f055ed0cc02128292f4fe2c91",
+    "url": "/quiz-khelo/index.html"
+  },
+  {
+    "revision": "08843f3b8ea5f5ba2c0f",
+    "url": "/quiz-khelo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ca6abf9ab404b10089c",
+    "url": "/quiz-khelo/static/js/2.8e1b23d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-khelo/static/js/2.8e1b23d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08843f3b8ea5f5ba2c0f",
+    "url": "/quiz-khelo/static/js/main.c36d2934.chunk.js"
+  },
+  {
+    "revision": "34aa0ffad09694ec44a0",
+    "url": "/quiz-khelo/static/js/runtime-main.d3656a3e.js"
+  },
+  {
+    "revision": "6c9699dbfe0b1860edde20f19bd003d0",
+    "url": "/quiz-khelo/static/media/image1.6c9699db.jpg"
+  }
+]);
